170edd4cff44ecc9836badd982fbd7954bc6fa58
tasiamac <160981455+tasiamac@users.noreply.github.com>
2024-07-14 18:03:11 -0400

    Add files via upload

--- savings_account.py
+++ savings_account.py
@@ -1,8 +1,17 @@
 """Import the Account class from the Account.py file."""
 # ADD YOUR CODE HERE
+from Account import Account
 
 # Define a function for the Savings Account
-def create_savings_account(balance, interest_rate, months):
+class SavingsAccount:
+    def create_savings_account(self, savings_balance, savings_interest_rate, savings_maturity, savings_apr):
+        self.balance = savings_balance
+        self.interest_rate = savings_interest_rate
+        self.months = savings_maturity
+        self.apr = savings_apr
+        update_balance = savings_balance
+        interest_earned = savings_interest_rate * savings_balance
+    
     """Creates a savings account, calculates interest earned, and updates the account balance.
 
     Args:
@@ -17,18 +26,24 @@ def create_savings_account(balance, interest_rate, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
+    Account = SavingsAccount(savings_balance)
 
     # Calculate interest earned
      # ADD YOUR CODE HERE
+    interest_rate = (savings_balance * (savings_apr/100 * savings_months/12))
 
     # Update the savings account balance by adding the interest earned
     # ADD YOUR CODE HERE
+    SavingsAccount.update_balance = savings_balance + savings_interest_rate
 
     # Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
+    SavingsAccount.update_balance
 
     # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
+    SavingsAccount.interest_earned
 
     # Return the updated balance and interest earned.
-    return  # ADD YOUR CODE HERE
+    # ADD YOUR CODE HERE
+    print("The balance is: $", format(savingsaccount_data.get_balance(), ',.2f'))
